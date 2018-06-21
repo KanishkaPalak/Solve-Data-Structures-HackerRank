9a7aa60f3693c232f27f31138551e5f92d2506d4
Kanishka Rao <kanishka.palak@gmail.com>
2018-06-21 21:36:09 +0530

    Create Print in Reverse.cpp

---
+++ Print in Reverse.cpp
@@ -0,0 +1,9 @@
+void ReversePrint(Node *head)
+{
+  // This is a "method-only" submission. 
+  // You only need to complete this method. 
+    if(head==NULL)
+        return;
+    ReversePrint(head->next);
+    cout<<head->data<<endl;
+}
