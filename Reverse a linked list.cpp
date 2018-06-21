946815b1ea4ab5933fa6f4689cc4895835feeebc
Kanishka Rao <kanishka.palak@gmail.com>
2018-06-21 21:36:30 +0530

    Create Reverse a linked list.cpp

---
+++ Reverse a linked list.cpp
@@ -0,0 +1,22 @@
+Node* Reverse(Node *head)
+{
+  // Complete this method
+    
+    if(head == NULL || head->next == NULL)
+        return head;
+    else
+    {
+        Node *prev = NULL;
+        Node *cur = head;
+        Node *next;
+        while(cur != NULL)
+        {
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        head = prev;
+        return head;
+    }
+}
